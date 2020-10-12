811739e28d173f9ac3d8e4cc5d684c4a367d3aca
Beauregard <46849790+beaucave@users.noreply.github.com>
2020-10-12 15:31:41 -0600

    t

---
+++ src/app/RobotTravel.java
@@ -0,0 +1,23 @@
+package app;
+
+public class RobotTravel {
+    public double m_Speed;
+    public double slowspeed = 0;
+    public double maxspeed = 10;
+
+    public RobotTravel(double targetSpeed) {
+        System.out.println("TextBot stands still, awaiting your command.");
+    }
+
+}
+
+
+
+    /*
+    shell code here
+
+    so, takes an input from the terminal (commandline) - "TURN LEFT"
+    has parameters set within the code for (a) TURN and (b) LEFT
+    when reading TURN LEFT, print string: "Robot has turned left by 90 degrees" or smthn
+
+    */
